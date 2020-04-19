8ad519927a10c60dd477ed88164a23f0efc4c412
Qucheng Miao <qcmiao@hotmail.com>
2020-04-19 15:30:26 +0800

    maybe alpha

---
+++ OpenEvaluation/Helpers/FileUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace OpenEvaluation.Helpers
+{
+    public static class FileUtil
+    {
+        public static async Task SaveAs(IJSRuntime js, string filename, byte[] data)
+        {
+            await js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+        }
+    }
+}
